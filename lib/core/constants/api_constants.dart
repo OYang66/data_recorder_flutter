377fef7d5bd975c40aba29cb41c74ea2cb5672c7
我不姓欧阳 <781696767@qq.com>
2026-05-20 16:56:54 +0800

    切换 iOS 可用 HTTPS 后端地址

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -3,6 +3,6 @@ class ApiConstants {
 
   static const baseUrl = String.fromEnvironment(
     'DATARECORDER_BASE_URL',
-    defaultValue: 'http://175.178.12.210:82/prod-api/',
+    defaultValue: 'https://yxff.work/prod-api/',
   );
 }
